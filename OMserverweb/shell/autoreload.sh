370875870237f0bda6a6372c4fbccbdca96bc777
babyll <yingcaiye@163.com>
2016-03-04 09:51:22 +0800

    增加测试

---
+++ OMserverweb/shell/autoreload.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+objectdir="/data/www/OMserverweb"
+/usr/bin/inotifywait -mrq --exclude "(static|logs|shell|\.swp|\.swx|\.pyc|\.py\~)" --timefmt '%d/%m/%y %H:%M' --format '%T %w%f' --event modify,delete,move,create,attrib ${objectdir} | while read files
+do
+    /bin/touch /data/www/OMserverweb/shell/reload.set
+    continue
+done &
+
+
+
+测试一下，看看好用不好用
+
+咋回事啊
---
+++ oms-master/oms-master/oms/settings_local.py
@@ -0,0 +1,34 @@
+SALT_API = {"url": "",
+            "user": "",
+            "password": ""
+            }
+
+Cobbler_API = {"url": "",
+            "user": "",
+            "password": ""
+            }
+
+# salt result
+RETURNS_MYSQL = {"host": "192.168.1.32",
+               "port": 3306,
+               "database": "salt",
+               "user": "yitong",
+               "password": "yitong2015"
+                }
+
+SERVICE = {"nginx": "nginx",
+           "php": "php",
+           "mysql": "mysql",
+           "sysinit": "sysinit",
+           "logstash": "logstash",
+           "zabbix": "zabbix",
+           "redis": "redis",
+           "memcached": "memcached"
+          }
+
+OMS_MYSQL = {"host": "192.168.1.32",
+               "port": 3306,
+               "database": "oms",
+               "user": "yitong",
+               "password": "yitong2015"
+                }
